ee22c9b578456575dde1c245dd35dc76b0f01791
thicco-catto <cristobal.martin01@gmail.com>
2022-04-19 18:24:15 +0200

    Satan gets angrier now

--- scripts/holy_smokes.lua
+++ scripts/holy_smokes.lua
@@ -208,7 +208,9 @@ end
 -- UPDATE CALLBACKS
 local function EndAttack()
     CurrentMinigameState = MinigameState.NO_ATTACK
-    MinigameTimers.NextAttackTimer = MinigameConstants.MAX_NO_ATTACK_FRAMES
+
+    MinigameTimers.NextAttackTimer = math.max(30, MinigameConstants.MAX_NO_ATTACK_FRAMES - math.floor((SatanHead.MaxHitPoints - SatanHead.HitPoints) / 4))
+    print(MinigameTimers.NextAttackTimer)
 end
 
 
