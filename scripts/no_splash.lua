0a5d1f9fdf4313ae0ecc92746bf8576545fb3380
thicco-catto <cristobal.martin01@gmail.com>
2022-06-29 20:03:16 +0200

    Added no splash stuff

--- scripts/no_splash.lua
+++ scripts/no_splash.lua
@@ -1,21 +1,71 @@
 local no_splash = {}
 local game = Game()
-
+local rng = RNG()
+local SFXManager = SFXManager()
+local MusicManager = MusicManager()
 ----------------------------------------------
---FANCY REQUIRE (Thanks manaphoenix <3)
+-- FANCY REQUIRE (Thanks manaphoenix <3)
 ----------------------------------------------
-local _, err = pcall(require, "")
-local modName = err:match("/mods/(.*)/%.lua")
-local path = "mods/" .. modName .. "/"
-
 local function loadFile(loc, ...)
+    local _, err = pcall(require, "")
+    local modName = err:match("/mods/(.*)/%.lua")
+    local path = "mods/" .. modName .. "/"
+
     return assert(loadfile(path .. loc .. ".lua"))(...)
 end
-
 local ArcadeCabinetVariables = loadFile("scripts/variables")
 
+no_splash.callbacks = {}
+no_splash.result = nil
+no_splash.startingItems = {}
+
+-- Sounds
+local MinigameSounds = {
+
+    WIN = Isaac.GetSoundIdByName("arcade cabinet win"),
+    LOSE = Isaac.GetSoundIdByName("arcade cabinet lose")
+}
+
+local MinigameMusic = Isaac.GetMusicIdByName("bsw black beat wielder")
+
+-- Entities
+local MinigameEntityTypes = {
+}
+
+local MinigameEntityVariants = {
+    BACKGROUND = Isaac.GetEntityVariantByName("background TGB"),
+}
+
+-- Constants
+local MinigameConstants = {
+}
+
+-- Timers
+local MinigameTimers = {
+}
+
+-- States
+local CurrentMinigameState = 0
+local MinigameState = {
+
+    LOSING = 5,
+    WINNING = 6,
+}
+
+-- UI
+local TransitionScreen = Sprite()
+TransitionScreen:Load("gfx/minigame_transition.anm2", true)
+
 function no_splash:Init()
-    print("Init no splash")
+    local overlay = Isaac.Spawn(EntityType.ENTITY_EFFECT, MinigameEntityVariants.BACKGROUND, 0, game:GetRoom():GetCenterPos(), Vector.Zero, nil)
+    overlay:GetSprite():Load("gfx/ns_overlay.anm2", true)
+    overlay:GetSprite():Play("Idle", true)
+    overlay.DepthOffset = 1000
+
+    local bg = Isaac.Spawn(EntityType.ENTITY_EFFECT, MinigameEntityVariants.BACKGROUND, 0, game:GetRoom():GetCenterPos(), Vector.Zero, nil)
+    bg:GetSprite():ReplaceSpritesheet(0, "gfx/grid/ns_bg.png")
+    bg:GetSprite():LoadGraphics()
+    bg.DepthOffset = -1000
 end
 
 no_splash.callbacks = {
