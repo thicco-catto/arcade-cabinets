afd14dc9537fe85f007783dc8d7adadfeec302c2
thicco-catto <cristobal.martin01@gmail.com>
2022-08-04 12:23:41 +0200

    Fixed sumb shit

--- main.lua
+++ main.lua
@@ -203,10 +203,10 @@ function ArcadeCabinetMod:OnNewLevel()
 
         local totalModRooms = arcadeRooms + secretRooms + otherRooms
         if totalModRooms == 0 then
-            print(totalRooms .. " were generated, out of which " .. totalModRooms .. " were modded")
+            print(totalRooms .. " rooms were generated, out of which " .. totalModRooms .. " were modded")
         else
             local modRoomsPercentage = totalModRooms / totalRooms * 100
-            print(totalRooms .. " were generated, out of which " .. totalModRooms .. " (".. modRoomsPercentage .."%) were modded, and out of which")
+            print(totalRooms .. " rooms swere generated, out of which " .. totalModRooms .. " (".. modRoomsPercentage .."%) were modded, and out of which")
 
             local arcadeRoomsPercentage = arcadeRooms / totalModRooms * 100
             print("-" .. arcadeRooms .. " were arcade rooms (" .. arcadeRoomsPercentage .. "%)")
